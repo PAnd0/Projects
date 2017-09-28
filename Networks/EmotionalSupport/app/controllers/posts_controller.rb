8d90359ec2bbb809afd885c222237daf47cab9f4
RCAVelez <chve38@gmail.com>
2017-09-28 11:31:20 -0500

    Added userid to post, used currentuserid to fill the userid in post in the create action of the post controller

--- Networks/EmotionalSupport/app/controllers/posts_controller.rb
+++ Networks/EmotionalSupport/app/controllers/posts_controller.rb
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
    # @post.id = Post.all.count + 1
    binding.pry
     respond_to do |format|
@@ -81,7 +82,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content,:user_id)
       
     end
 end
