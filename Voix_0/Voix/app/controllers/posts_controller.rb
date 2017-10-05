b4214cc31aff4a5f02fc25c5b8d872dd6f03db26
RCAVelez <chve38@gmail.com>
2017-10-05 12:10:11 -0500

    About to add the comments

--- Voix_0/Voix/app/controllers/posts_controller.rb
+++ Voix_0/Voix/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
@@ -27,14 +28,15 @@ class PostsController < ApplicationController
   def create
    
     @post = Post.new(post_params)
-   
+    @post.user_id = current_user.id
+  
    
    
       if @post.save
-        redirect_to root_path
+        redirect_to posts_path
       else
         
-       
+       ##Flash notice something
       end
     
   end
--- Voix_0/Voix/config/routes.rb
+++ Voix_0/Voix/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-	root 'posts#index'
+  root 'home#index'
+
+  devise_for :users, controllers: { registrations: "registrations"}
+
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
